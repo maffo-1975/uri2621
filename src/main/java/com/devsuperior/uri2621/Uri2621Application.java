43cbd66417a1f91ed4a11b34be30873245f934bf
Marcio Cavalcante <mca19ntl@gmail.com>
2022-02-13 18:16:15 -0300

    Soon after class

---
+++ src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -0,0 +1,38 @@
+package com.devsuperior.uri2621;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
+import com.devsuperior.uri2621.repositories.ProductRepository;
+
+@SpringBootApplication
+public class Uri2621Application implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2621Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<ProductMinProjection> list1 = repository.search1(10, 20, "P");
+		System.out.println("\n*** RESULTADO SQL RAIZ:");
+		list1.stream().map(x -> new ProductMinDTO(x)).forEach(System.out::println);
+
+		System.out.println("\n\n");
+
+		List<ProductMinDTO> list2 = repository.search2(10, 20, "P");
+		System.out.println("\n*** RESULTADO JPQL:");
+		list2.stream().forEach(System.out::println);
+
+	}
+
+}
